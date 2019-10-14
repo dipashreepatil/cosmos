f44b6c97d2796cdff7f20017040d9034548e1e7f
dipashreepatil <44296051+dipashreepatil@users.noreply.github.com>
2019-10-14 12:48:20 +0530

    Create for_loop.c

---
+++ code/language/c/loop/for_loop.c
@@ -0,0 +1,11 @@
+/* for loop is used in c where initial condion , final condition are known and variable is incremented or decremented step wise.*/
+//example to print numbers in range 11 to 20 :
+
+void main()
+{
+int i;
+  for(i=11;i<=20;i++)
+  {
+   printf("%d",i);
+  }
+}
